4befdd7edd74b54f7242684c8d428068e5318ac7
fokrul511 <islamfokrul2002@gmail.com>
2024-05-04 10:42:32 +0600

    create splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
+import 'package:crafty_bay_ecomarc_apps/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'presentation/utility/apps_colors.dart';
 
 class CarftyBay extends StatelessWidget {
   const CarftyBay({super.key});
@@ -7,7 +10,14 @@ class CarftyBay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.primaryColor,
+        progressIndicatorTheme: const ProgressIndicatorThemeData(
+          color: AppColors.primaryColor,
+        ),
+      ),
     );
   }
 }
--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,5 +1,8 @@
+import 'package:crafty_bay_ecomarc_apps/presentation/screens/email_verification_screen.dart';
+import 'package:crafty_bay_ecomarc_apps/presentation/utility/images_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,17 +12,32 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Get.to(() => const EmailVerificationScreen());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Spacer(),
-          SvgPicture.asset(
-            'assets/images/logo.svg',
-            width: 100,
-          )
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            SvgPicture.asset(AssetsPath.appLogoSvg, width: 100),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const SizedBox(height: 16),
+            const Text("Version 1.0"),
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
