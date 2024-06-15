98a27eae74d1cd9acc221149bdb6ad0d58741d95
fokrul511 <islamfokrul2002@gmail.com>
2024-06-15 11:23:45 +0600

    - added Cart list

--- lib/presentation/screens/cart_list_screen.dart
+++ lib/presentation/screens/cart_list_screen.dart
@@ -1,9 +1,17 @@
 import 'package:crafty_bay_ecomarc_apps/presentation/state_holders/main_bottom_nav_bar_controller.dart';
+import 'package:crafty_bay_ecomarc_apps/presentation/utility/apps_colors.dart';
+import 'package:crafty_bay_ecomarc_apps/presentation/widgets/cart_product_item.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class CartListScreen extends StatelessWidget {
+class CartListScreen extends StatefulWidget {
   const CartListScreen({super.key});
+
+  @override
+  State<CartListScreen> createState() => _CartListScreenState();
+}
+
+class _CartListScreenState extends State<CartListScreen> {
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +30,68 @@ class CartListScreen extends StatelessWidget {
             icon: const Icon(Icons.arrow_back_ios),
           ),
         ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return CardProductItem();
+                },
+                itemCount: 10,
+              ),
+            ),
+            _buildCheckoutWidgets()
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildCheckoutWidgets() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: AppColors.primaryColor.withOpacity(0.1),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16),
+            topRight: Radius.circular(16),
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildTotalPrice(),
+          SizedBox(
+            width: 100,
+            child: ElevatedButton(
+              child: Text("CheckOut"),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTotalPrice() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Total Price",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+        Text(
+          "\$12000",
+          style: TextStyle(
+            fontSize: 24,
+            color: AppColors.primaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/presentation/widgets/cart_product_item.dart
@@ -0,0 +1,127 @@
+import 'package:crafty_bay_ecomarc_apps/presentation/utility/apps_colors.dart';
+import 'package:crafty_bay_ecomarc_apps/presentation/utility/images_path.dart';
+import 'package:flutter/material.dart';
+import 'package:item_count_number_button/item_count_number_button.dart';
+
+class CardProductItem extends StatefulWidget {
+  const CardProductItem({super.key});
+
+  @override
+  State<CardProductItem> createState() => _CardProductItemState();
+}
+
+class _CardProductItemState extends State<CardProductItem> {
+  int _countValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      surfaceTintColor: Colors.white,
+      elevation: 4,
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      child: Row(
+        children: [
+          _buildProductImage(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _buildProductDetails(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductDetails() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildProductName(),
+                  Wrap(
+                    spacing: 16,
+                    children: [
+                      Text(
+                        'Color: Red',
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Text(
+                        'Size: XL',
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete_outline_sharp),
+            )
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '\$100',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: AppColors.primaryColor,
+              ),
+            ),
+            _buildCounter(),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCounter() {
+    return ItemCount(
+      initialValue: _countValue,
+      minValue: 1,
+      maxValue: 20,
+      decimalPlaces: 0,
+      color: AppColors.primaryColor,
+      onChanged: (value) {
+        _countValue = value as int;
+        setState(() {});
+      },
+    );
+  }
+
+  Widget _buildProductName() {
+    return Text(
+      'Nike shoe 12345685',
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+    );
+  }
+
+  Widget _buildProductImage() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Image.asset(
+        AssetsPath.productDummyImagePng,
+        width: 100,
+      ),
+    );
+  }
+}
